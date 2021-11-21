8ab0b14f500d813c199927e95c7b7f2f920ba05e
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-11-21 00:04:42 -0300

    feat: changed neomat to flakes and updated syntax

--- global-config.nix
+++
@@ -1,8 +0,0 @@
-rec {
-  username = "matdsoupe";
-  email = "matheus_pessanha2001@outlook.com";
-  selected-desktop-environment = "gnome";
-  rootPath = builtins.toString ./.; 
-  rooPathNix = rootPath;
-  flake = import ./lib/flake { };
-}
